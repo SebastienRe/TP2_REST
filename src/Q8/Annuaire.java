453ffbb48e7ac7e46386caab79a3a7cd6537e13c
Sebastien <sebastien.re@alumni.univ-avignon.fr>
2024-03-20 10:05:46 +0100

    Q8

---
+++ src/Q8/Annuaire.java
@@ -0,0 +1,87 @@
+package Q8;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/")
+public class Annuaire {
+    private static Carnet carnet = new Carnet();
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getCarnet() {
+        if (carnet.isEmpty()) {
+            return "Liste vide";
+        } else {
+            return carnet.toString();
+        }
+    }
+
+    @GET
+    @Path("/{nom}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getNumero(@PathParam("nom") String nom) {
+        System.out.println("nom : " + nom);
+        String numero = carnet.getNumeroWithNom(nom);
+        if (numero == null) {
+            return "Inconnu";
+        } else {
+            return numero;
+        }
+    }
+
+    @POST
+    @Path("/addContact")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.TEXT_HTML)
+    public String createContact(Contact contact) {
+        String doigts = "nom : " + contact.getNom() + " - numero : " + contact.getNumero();
+        if (contact.getNom() == null || contact.getNumero() == null) {
+            return "<p>Erreur : nom ou numero manquant </p>" + doigts;
+        } else if (carnet.contains(contact.getNom())) {
+            return "<p>Erreur : contact déjà existant</p>" + doigts;
+        } else {
+            carnet.addContact(contact.getNom(), contact.getNumero());
+            return "<a href=\"" + contact.getNom() + "\">Fiche contact</a>" + doigts;
+        }
+    }
+
+    @PUT
+    @Path("updateContact/{nom}")
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.TEXT_HTML)
+    public Response updateContact(@PathParam("nom") String nom, Contact updatedContact) {
+        System.out.println("nom : " + nom + " - numero : " + updatedContact.getNumero());
+        if (carnet.isEmpty() || !carnet.contains(nom)) {
+            System.out.println("Contact introuvable");
+            return Response.noContent().build(); // 204
+        } else {
+            System.out.println("Contact trouvé");
+            carnet.updateContact(nom, updatedContact.getNumero());
+            return Response.ok().build(); // 200
+        }
+    }
+
+    @DELETE
+    @Path("/deleteContact/{nom}")
+    @Produces(MediaType.TEXT_HTML)
+    public Response deleteContact(@PathParam("nom") String nom) {
+        if (carnet.isEmpty() || !carnet.contains(nom)) {
+            System.out.println("Contact introuvable");
+            return Response.noContent().build(); // 204
+        } else {
+            System.out.println("Contact supprimé");
+            carnet.deleteContact(nom);
+            return Response.ok().build(); // 200
+        }
+    }
+
+}
---
+++ src/Q8/client/RestClient.java
@@ -0,0 +1,28 @@
+package Q8.client;
+
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.core.Response;
+
+public class RestClient {
+
+    public static void main(String[] args) {
+        try {
+            Client client = ClientBuilder.newClient();
+
+            Response response = client.target("http://localhost:8080/Q8/carnet/deleteContact/Jane")
+                    .request()
+                    .delete();
+
+            String message = response.readEntity(String.class);
+            // display response status
+            System.out.println(response.getStatus());
+            System.out.println(message);
+
+            response.close();
+            client.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
